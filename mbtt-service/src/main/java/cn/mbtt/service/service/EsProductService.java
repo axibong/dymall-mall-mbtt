95cacf8eb45ea7b4c1f7400668b0c674240ef283
bong <axibong63@gmail.com>
2025-02-20 19:54:00 +0800

    合并4.0（完成了订单模块）

---
+++ mbtt-service/src/main/java/cn/mbtt/service/service/EsProductService.java
@@ -0,0 +1,17 @@
+//package cn.mbtt.service.service;
+//
+//import cn.mbtt.service.domain.po.Products;
+//import org.springframework.data.domain.Page;
+//
+//public interface EsProductService {
+//
+//
+//    int importAll();
+//
+//    void delete(Long id);
+//
+//    Page<Products> search(String keyword, Integer pageNum, Integer pageSize);
+//
+//    Page<Products> preciseSearch(String keyword, Integer sortType, Integer pageNum, Integer pageSize);
+//}
+//
